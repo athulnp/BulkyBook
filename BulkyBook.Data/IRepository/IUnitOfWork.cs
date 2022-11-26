ce3af09bdcf89ca8e4bb9bd482456ef412bee2b1
Athul Nalupurakkal <athulonline13@gmail.com>
2022-11-26 12:56:31 +0530

    Added Covertype

--- BulkyBook.Data/IRepository/IUnitOfWork.cs
+++ BulkyBook.Data/IRepository/IUnitOfWork.cs
@@ -9,6 +9,7 @@ namespace BulkyBook.Data.IRepository
     public interface IUnitOfWork
     {
         ICategoryRepository Category {get;}
+        ICoverTypeRepository CoverType {get;}
         void Save();
     }
 }
--- BulkyBook.Data/Repository/UnitOfWork.cs
+++ BulkyBook.Data/Repository/UnitOfWork.cs
@@ -5,11 +5,13 @@ namespace BulkyBook.Data.Repository
     public class UnitOfWork : IUnitOfWork
     {
         public ICategoryRepository Category { get; private set; }
+        public ICoverTypeRepository CoverType { get; private set; }
         private readonly ApplicationDbContext _db;
         public UnitOfWork(ApplicationDbContext db)
         {
             _db = db;
             Category = new CategoryRepository(_db);
+            CoverType = new CoverTypeRepository(_db);
         }
         public void Save()
         {
